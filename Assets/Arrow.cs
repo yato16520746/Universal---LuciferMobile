b3f23ae9f3988e030bc112621fa3a24fb12e7b40
Nguyễn Văn Minh <16520746@gm.uit.edu.vn>
2020-05-02 22:20:53 +0700

    Skeleton bắn được Cung!

---
+++ Assets/Arrow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    [SerializeField] float _speed = 50;
+    [SerializeField] float _delay = 0.1f;
+    bool canFly = false;
+
+    private void Start()
+    {
+        StartCoroutine(CanFly());
+    }
+
+    private void Update()
+    {
+        if (canFly)
+            transform.Translate(Vector3.up * _speed * Time.deltaTime);
+    }
+
+    IEnumerator CanFly()
+    {
+        yield return new WaitForSeconds(_delay);
+
+        canFly = true;
+    }
+}
---
+++ Assets/SkeletonB_Delegate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkeletonB_Delegate : MonoBehaviour
+{
+    [SerializeField] Transform _arrowTransform;
+    [SerializeField] GameObject _arrowPref;
+
+    public void ShootArrow()
+    {
+        GameObject arrow = Instantiate(_arrowPref, _arrowTransform.position, _arrowTransform.rotation);        
+    }
+}
