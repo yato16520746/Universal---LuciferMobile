7fb724ab7cd859dd7bd28337c8220c0fc7dcf736
Nguyễn Văn Minh <16520746@gm.uit.edu.vn>
2020-07-12 21:21:27 +0700

    Chuẩn bị thiết kế màn chơi

---
+++ Assets/Scripts/Paladin/SpecialSword.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpecialSword : MonoBehaviour
+{
+    enum SwordState
+    {
+        FollowPaladin,
+        LookPlayer,
+        AttackPlayer,
+
+    }
+
+    [SerializeField] Transform _paladinTransform;
+    [SerializeField] Transform _followTransform;
+
+    [SerializeField] Rigidbody _rb;
+    [SerializeField] float _speed = 10f;
+    [SerializeField] float _speedLerp = 4f;
+
+    SwordState _state;
+
+    void Start()
+    {
+        _state = SwordState.FollowPaladin;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_state == SwordState.FollowPaladin)
+        {
+            Vector3 direction = _followTransform.position - transform.position;
+            if (direction.magnitude > 1f)
+            {
+
+
+                Vector3 velocity = direction.normalized * _speed;
+
+                _rb.velocity = Vector3.Lerp(_rb.velocity, velocity, _speedLerp * Time.deltaTime);
+            }
+        }
+        else
+        {
+            // look player
+            Player player = Player.Instance;
+            Vector3 targetPos = player.transform.position + new Vector3(0, 1.87562f / 2f, 0);
+
+            Vector3 vector = targetPos - transform.position;
+
+            Quaternion rotation = Quaternion.LookRotation(vector);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, 10f * Time.deltaTime);
+        }
+
+
+    }
+}
